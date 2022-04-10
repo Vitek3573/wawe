33d7dd7db7e92468813bf7df6c6197a5d7459e5e
Vitek3573 <vitya_fetisov@mail.ru>
2022-04-10 15:48:34 +0300

    create project

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,10 @@
-$(function(){
+$(function(){ 
+    $(".menu a").on("click", function (e) {   
+		e.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
     var mixer = mixitup('.portfolio__content');
     $('.slider-blog__inner').slick({
         dots: true,
